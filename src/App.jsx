44c5e583c29d1a273c1c306a34a03b2520a84a99
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-17 16:57:30 +0100

    final commit 17-01

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import MeteoMainSection from './components/MeteoMainSection'
 
 
 
+// TODO far diventare app la mainpage
+// Rendere MeteoOdierno interactive per ricevere solo il meteo di oggi
+// Dividere la sezione MeteoNextDays Per giorno 
+// Ricerca avanzata tramite anche nazione 
+//  Miglioramento background image in base al tempo
+
+
 function App() {
 
 
